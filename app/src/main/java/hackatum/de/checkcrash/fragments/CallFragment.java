7c8d42fe86fcb8dba7184203c52f7e3abf8b7b56
Basti Destruction <qwertz1029384756@gmail.com>
2016-11-12 16:43:22 +0100

    Call Fragment implementation

--- app/src/main/java/hackatum/de/checkcrash/fragments/CallFragment.java
+++ app/src/main/java/hackatum/de/checkcrash/fragments/CallFragment.java
@@ -1,13 +1,21 @@
 package hackatum.de.checkcrash.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 import hackatum.de.checkcrash.R;
+import hackatum.de.checkcrash.SystemOverlay;
 import hackatum.de.checkcrash.models.AccidentProcedure;
 import hackatum.de.checkcrash.models.Page;
 
@@ -17,6 +25,7 @@ public class CallFragment extends Fragment {
     private String pageId;
 
     private PageFragmentListener mListener;
+    private ImageButton callButton;
 
     public CallFragment() {
         // Required empty public constructor
@@ -44,9 +53,39 @@ public class CallFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_call, container, false);
 
         Page page = AccidentProcedure.accidentProcedure.pages.get(pageId);
+
+        callButton = (ImageButton) view.findViewById(R.id.button_call);
+        TextView questionText = (TextView) view.findViewById(R.id.text_question);
+
+        final String uri;
+        if (page.shortDesc.equals("Police")) {
+            uri = "tel:" + AccidentProcedure.accidentProcedure.policeNumber;
+            callButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_police_cap_black));
+        } else {
+            uri = "tel:" + AccidentProcedure.accidentProcedure.emergencyNumber;
+            callButton.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_ambulance_black));
+        }
+
+        questionText.setText(page.question);
+        page.speak(getContext(), Locale.ENGLISH);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivityForResult(intent, 70);
+                createOverlay();
+            }
+        });
         mListener.onPageLoad(page);
 
         return view;
+    }
+
+    public void createOverlay() {
+        //SystemOverlay.requestSystemAlertPermission(getActivity()); // TODO: 12.11.2016 call in settings
+        SystemOverlay.createPhoneOverlay(getContext(), "noot noot noot", "noot noot", "noot");
     }
 
 
@@ -65,5 +104,12 @@ public class CallFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 70) {
+            SystemOverlay.destroyPhoneOverlay();
+        }
     }
 }
